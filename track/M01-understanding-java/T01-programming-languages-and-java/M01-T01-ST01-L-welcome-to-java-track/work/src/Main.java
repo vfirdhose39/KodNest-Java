eca8e639a20efc2cfed832ff7bba634cfa57dee6
vfirdhose11 <vfirdhose11@gmail.com>
2026-08-05 12:25:35 +0530

    day1changes

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Main.java
@@ -0,0 +1,11 @@
+// This program prints a formatted student ID card profile to the console.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("-------------------------");
+        System.out.println("STUDENT PROFILE");
+        System.out.println("Name: Alex");
+        System.out.println("Course: Java");
+        System.out.println("Level: Beginner");
+        System.out.println("-------------------------");
+    }
+}
