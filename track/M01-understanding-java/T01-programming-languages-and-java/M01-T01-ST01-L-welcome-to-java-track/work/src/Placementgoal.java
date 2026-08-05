89d0e30e9a817f1d3427c96fef45201f1ab3a87f
vfirdhose11 <vfirdhose11@gmail.com>
2026-08-05 12:20:55 +0530

    day1changes

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Placementgoal.java
@@ -0,0 +1,10 @@
+public class Placementgoal {
+    public static void main(String[] args) {
+        // This program prints the user's placement goals, including desired role, target, and daily commitment.
+        System.out.println("---MY PLACEMENT GOAL ---");
+        System.out.println("Role: Software Developer");
+        System.out.println("Target: Get Placed");
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
+    
