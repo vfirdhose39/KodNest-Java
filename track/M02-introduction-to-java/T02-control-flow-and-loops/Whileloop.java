b6a19662636180a7c1b5997e64e40593b173a029
vfirdhose11 <vfirdhose11@gmail.com>
2026-08-09 12:19:08 +0530

    updates

---
+++ track/M02-introduction-to-java/T02-control-flow-and-loops/Whileloop.java
@@ -0,0 +1,18 @@
+public class Whileloop {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while (i <= 5) { 
+            int j = 0; 
+            
+            while (j <= 5) { 
+                System.out.println(j);
+                j++; 
+            }
+            
+            System.out.println();
+            i++; 
+        }
+    }
+}
+ 
